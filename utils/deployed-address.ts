ad893c04f7971a16b6e110686635c3e25d53fdc2
jiyon-dr w <passaupo44@gmail.com>
2024-08-14 07:11:37 +0000

    feat: initiated the project

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xa6dC9a3c1f37214909eEa485b16ef4C5d0f1dBAd'
+export const ProxyAdmin = '0xC67BF64CDC0826b709200ad0B861A4b05bb4443e'
+export const SWTRImplementation = '0x8dB55B6b26fA5927E2047df7ebA4130Bf8d99C39'
     
